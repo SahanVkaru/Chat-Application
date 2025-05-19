ed5bd4c8c97d5538b655ccf37e1311f923171c29
A.B.D. ANANDAKUMARA <116487199+BuddhikaBICT-UoR-FoT-6@users.noreply.github.com>
2025-05-19 08:06:14 +0530

    Update UserProfileService.java

--- Chat Application/src/main/java/com/chatapp/service/UserProfileService.java
+++ Chat Application/src/main/java/com/chatapp/service/UserProfileService.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 /**
- * Service class for User profile related operations
+ * Service class for User profile related operations which can be done via the business layer of this application
  */
 public class UserProfileService {
 
